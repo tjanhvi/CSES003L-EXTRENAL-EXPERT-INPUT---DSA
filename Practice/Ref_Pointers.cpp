ad92d97f8f8d11a40ffb5f647461b22036c11909
JANHVI TIWARI <tjanhvi2003@outlook.com>
2023-06-11 00:27:10 +0530

    Added Reference_pointer code

---
+++ Practice/Ref_Pointers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+  int var = 10;
+  cout << &var << endl;
+
+  int *ptr;
+  ptr =  &var;
+
+  cout << ptr << endl;
+  cout << &ptr << endl;
+  cout << *ptr << endl;
+
+
+  return 0;
+}
